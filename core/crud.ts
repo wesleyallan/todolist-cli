3383d0ce0a949f19cbe9e36673f1110bfb339075
Wesley Silva <wesley.allansilva@gmail.com>
2023-12-28 17:58:46 -0300

    feat: add update in crud

--- core/crud.ts
+++ core/crud.ts
@@ -11,7 +11,7 @@ interface Todo {
   done: boolean;
 };
 
-function create(content: string) {
+function create(content: string): Todo {
   const todo: Todo = {
     id: uuid(),
     date: new Date().toISOString(),
@@ -28,15 +28,34 @@ function create(content: string) {
     todos: todos ?? [],
     dogs: []
   }, null, 2));
-  return content;
+
+  return todo;
 }
 
-function read() {
+function read(): Array<Todo> {
   const dbString = fs.readFileSync(DB_FILE_PATH, 'utf-8');
   const db = JSON.parse(dbString || "{}");
   if (!db.todos) return [];
   return db.todos;
 }
+
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+  let todoUpdated;
+  const todos = read();
+  todos.forEach((currentTodo) => {
+    const isToUpdate = currentTodo.id === id;
+    if (isToUpdate) {
+      todoUpdated = Object.assign(currentTodo, partialTodo);
+    };
+  });
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos}, null, 2));
+  if (!todoUpdated) throw new Error("Please, provider another ID!");
+  return todoUpdated;
+};
+
+function updateContentById(id: string, content: string): Todo {
+  return update(id, {content});
+};
 
 function clearDb() {
   fs.writeFileSync(DB_FILE_PATH, "");
@@ -47,4 +66,10 @@ function clearDb() {
 clearDb();
 create("Primeira TODO");
 create("Segunda TODO!");
+const terceiraTodo = create("Terceira TODO!");
+update(terceiraTodo.id, {
+  content: "Terceira TODO Atualizada!",
+  done: true
+})
+updateContentById(terceiraTodo.id, "Terceira TODO duplamente atualizada");
 console.log(read());
